0dda2da0f954f30b5e19104486bee99232081b93
Nguyen Tri Thanh <nguyentrithanh1311@gmail.com>
2025-05-26 23:09:47 +0700

    Update code 26/5/2025.

---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>Hello
+      <div>
+        <Button type="primary">click me</Button>
+      </div>
+    </div>
+  );
+}
