97ab4e8b47c198f5f1717795e35e46107201e8bf
Nguyen Tri Thanh <nguyentrithanh1311@gmail.com>
2025-05-29 12:47:45 +0700

    Update code 29/5/2025.

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    // https://docs.nestjs.com/techniques/configuration#disable-env-variables-loading
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // https://docs.nestjs.com/techniques/mongodb#async-configuration
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
